1b4335a038f1e96dc48c7ccd80a4aad27d1137f6
Nefelin <flcarmonac@yahoo.com>
2023-03-16 13:32:48 -0300

    routes

--- routes/routes.js
+++ routes/routes.js
@@ -4,8 +4,26 @@ import * as func from "../utils/funciones.js";
 const myRouter = Router();
 
 myRouter.get("/", (req, res) => {
-    func.leerArchivoEquipo().then(data => {
+    func.leerArchivoCircuitos().then(data => {
         res.render("main", data);
+    });
+});
+
+myRouter.get("/carrera", (req, res) => {
+    func.leerArchivoCircuitos().then(data => {
+        res.render("carrera", data);
+    });
+});
+
+myRouter.get("/calendario", (req, res) => {
+    func.leerArchivoCircuitos().then(data => {
+        res.render("calendario", data);
+    });
+});
+
+myRouter.get("/posiciones", (req, res) => {
+    func.leerArchivoCircuitos().then(data => {
+        res.render("posiciones", data);
     });
 });
 
