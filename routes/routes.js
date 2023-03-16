ef57839f5ea8d17809ee68b684a37b7992288ca9
Sasdalorian <esteban.nicolas.sd@gmail.com>
2023-03-16 13:00:13 -0300

    footer, y banderas ordenadas

--- routes/routes.js
+++ routes/routes.js
@@ -4,14 +4,10 @@ import * as func from "../utils/funciones.js";
 const myRouter = Router();
 
 myRouter.get("/", (req, res) => {
-<<<<<<< HEAD
     func.leerArchivoCircuitos().then(data => {
         console.log(data);
         res.render("main", data);
     });
-=======
-    res.render("main");
->>>>>>> Sas
 });
 
 export default myRouter;
