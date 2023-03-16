1545677cdb43c5f73769f9cebb723f647eaa10c4
Nefelin <flcarmonac@yahoo.com>
2023-03-16 12:17:07 -0300

    Update

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,22 @@
 import { Router } from "express";
+import * as fs from "fs"
+
 const myRouter = Router();
 
 
 myRouter.get("/", (req, res) => {
-    res.send("PRUEBA");
+    leerArchivoCircuitos().then(data => {
+        console.log(data);
+        res.render("main", data);
+    });
 });
+
+async function leerArchivoCircuitos() {
+    const data = await fs.promises.readFile("./data/circuitos.json", (err, data) => {
+        if (err) throw err 
+        return data
+    });
+    return await JSON.parse(data);
+}
 
 export default myRouter;
