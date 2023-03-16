b5a5e082c88b40e55bbcf19df73ebaa646dd087f
Nefelin <flcarmonac@yahoo.com>
2023-03-16 10:14:28 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sprint Grupal F1
+
+> Install packages
+
+```
+npm install express
+npm install hbs
+npm init
+```
+* Bryan Jeldes
+* Nicolas Sanhueza
+* Francisco Carmona
---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import hbs from "hbs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import routes from "./routes/routes.js"
+
+const app = express();
+const _root = import.meta.url;
+const _dirname = dirname(fileURLToPath(_root));
+
+app.listen(3000);
+app.set("view engine", "hbs");
+hbs.registerPartials(join(_dirname, "/views/partials"));
+app.use(express.static("public"));
+app.use(routes)
