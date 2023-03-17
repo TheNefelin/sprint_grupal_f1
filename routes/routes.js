39c4da45f3f4774d241c46bd2d6a94eaffa38db6
TheNefelin <metalflap@gmail.com>
2023-03-16 21:08:41 -0300

    main

--- routes/routes.js
+++ routes/routes.js
@@ -4,27 +4,29 @@ import * as func from "../utils/funciones.js";
 const myRouter = Router();
 
 myRouter.get("/", (req, res) => {
-    func.leerArchivoEquipo().then(data => {
+    func.tablaCarrera().then(data => {
+        console.log(data)
         res.render("main", data);
-    });
-});
-
-myRouter.get("/carrera", (req, res) => {
-    func.leerArchivoEquipo().then(data => {
-        res.render("carrera", data);
     });
 });
 
 myRouter.get("/calendario", (req, res) => {
     func.leerArchivoCircuitos().then(data => {
         res.render("calendario", data);
-        console.log(data)
     });
 });
 
-myRouter.get("/posiciones", (req, res) => {
-    func.leerArchivoCircuitos().then(data => {
-        res.render("posiciones", data);
+myRouter.get("/carrera/:id", (req, res) => {
+    if (!isNaN(req.params.id)) {
+        func.prepararCarrera(parseInt(req.params.id)).then(data => {
+            // console.log(data)
+        });
+    };
+
+
+
+    func.leerArchivoEquipo().then(data => {
+        res.render("carrera", data);
     });
 });
 
--- utils/funciones.js
+++ utils/funciones.js
@@ -18,10 +18,44 @@ export async function leerArchivoEquipo() {
     return await JSON.parse(data);
 };
 
-export async function prepararCarrera() {
+export async function prepararCarrera(idCarrera) {
+    const circuito = await leerArchivoCircuitos();
+    const equipo = await leerArchivoEquipo();
+    let carrera = circuito.carrera.filter(e => e.id == idCarrera);
+
+    console.log(carrera)
+
+    if (circuito.isActive) {
+        return [circuito, equipo];
+    } else {
+        return [circuito, [false]]
+    }
     
-}
+    // Promise.all([
+    //     leerArchivoCircuitos(),
+    //     leerArchivoEquipo(),
+    // ]).then(data => {
+    //     data[0] = data[0].carrera.filter(e => e.id == idCarrera);
+    //     return data;
+    // });
+};
 
 export async function iniciarCarrera() {
     
+}
+
+export async function tablaCarrera() {
+    const circuito = await leerArchivoCircuitos();
+    const equipo = await leerArchivoEquipo();
+    const arrTablaCarrera = {circuito: [], equipo: []};
+
+    circuito.carrera.forEach(e => {
+        arrTablaCarrera.circuito.push({img: e.flag})
+    });
+
+    equipo.equipos.forEach(e => {
+        arrTablaCarrera.equipo.push({img: e.team})
+    });
+
+    return arrTablaCarrera;
 }
