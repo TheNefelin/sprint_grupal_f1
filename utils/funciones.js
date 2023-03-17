2b2ffd8f170a255ca41108e11687ddf8cba720cf
bryanjv <bryan.jeldes@gmail.com>
2023-03-17 11:27:44 -0300

    Agregar prob a utils/funciones.js

--- utils/funciones.js
+++ utils/funciones.js
@@ -115,3 +115,44 @@ export async function tablaCarrera() {
     });
     return arrTablaCarrera;
 }
+
+//Funcion encontrar celda
+function findCeil(arr, r, l, h) {
+    let mid;
+    while (l < h) {
+        mid = l + ((h - l) >> 1);
+        (r > arr[mid]) ? (l = mid + 1) : (h = mid);
+    }
+    return (arr[l] >= r) ? l : -1;
+}
+
+//Funcion randomixer
+function myRand(arr, freq, n) {
+    let prefix = [];
+    let i;
+    prefix[0] = freq[0];
+    for (i = 1; i < n; ++i)
+        prefix[i] = prefix[i - 1] + freq[i];
+    let r = Math.floor((Math.random() * prefix[n - 1])) + 1;
+
+    let indexc = findCeil(prefix, r, 0, n - 1);
+    return arr[indexc];
+}
+
+//Funcion padre de calculo de probabilidad.
+function randomprob(probj) {
+    let arr = [];
+    let freq = [];
+    let i;
+    let resultado = '';
+
+    probj.forEach(element => {
+        arr.push(element.id);
+        freq.push(element.prob);
+    });
+
+    resultado = myRand(arr, freq, arr.length);
+    console.log(probj[resultado]);
+
+    return probj[resultado];
+}
